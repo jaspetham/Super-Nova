ea9be4994e168cbe711cf0267b71473b4eca00ba
jaspetham <jaspertham98@gmail.com>
2022-09-13 18:56:37 +0800

    update page

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const header = document.querySelector('header');
+const mainContent = document.getElementById('mainContent');
 
 window.addEventListener('scroll', function(){
     const scroll = window.pageYOffset;
@@ -8,3 +9,58 @@ window.addEventListener('scroll', function(){
         header.classList.remove('header-change');
     }
 })
+
+mainContent.style.setProperty('top',`calc(100vh - ${header.offsetHeight}px)`);
+
+const plans = document.querySelectorAll('.plan-btn');
+const planPill = document.getElementById('plan-pill');
+
+plans.forEach((plan) =>{
+    plan.addEventListener('click', (e) =>{
+        setPlan(plan)
+    })
+})
+
+const details = document.querySelectorAll('.plan-details .detail');
+
+details.forEach((detail) =>{
+    detail.addEventListener('click', (e) =>{
+        setPlan(detail)
+    })
+})
+
+function setPlan(target){
+    removePlanActive();
+    var planId = target.getAttribute('plan-id');
+    addPlanActive(planId);
+    // move pill
+    switch(planId){
+        case '1':
+            movePlanPill('first');
+            break;
+        case '2':
+            movePlanPill('second');            
+            break;
+        case '3':
+            movePlanPill('third');            
+            break;
+    }
+}
+
+function removePlanActive(){
+    planPill.removeAttribute('class');
+    plans.forEach((plan) =>{
+        plan.classList.remove('active');
+    })
+    details.forEach((detail) =>{
+        detail.classList.remove('active');
+    })
+}
+
+function addPlanActive(id){
+    document.getElementById(`plan-${id}`).classList.add('active');
+    document.getElementById(`detail-${id}`).classList.add('active');
+}
+function movePlanPill(val){
+    planPill.classList.add(val);
+}
